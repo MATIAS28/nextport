742412410a893f9a5bf703d4b0e74b007be61f56
MATIAS28 <matiasmuoz22@yahoo.com>
2023-04-24 19:45:40 -0300

    CCU

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91847dd5b7b08726"; }
+/******/ 		__webpack_require__.h = function() { return "10d9f82c4715530b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/carousel.js
+++ components/carousel.js
@@ -7,10 +7,10 @@ export const  Carousel = ({imgs}) => {
         <div>
           <img className="d-block w-100 rounded-top pb-2" src={imgs[img-1]} alt=""/>
           <div className="d-flex justify-content-center p-4 border-bottom">
-            <div style={{height: '3rem'}} className="d-flex align-items-center bg-primary rounded">
-              <h4 className="m-0 p-2 text-light" onClick={() => setImg(img-1 < 1 ? 1 : img-1)}>{'<'}</h4>
-              <h6 style={{height: 'inherit', alignItems: 'center'}} className="d-flex m-0 p-2 bg-light">{img+'/'+imgs.length}</h6>
-              <h4 className="m-0 p-2 text-light" onClick={() => setImg(img+1 > imgs.length ? 1 : img+1)}>{'>'}</h4>
+            <div style={{height: '3rem'}} className="d-flex align-items-center bg-dark rounded shadow-sm">
+              <h4 className="m-0 p-2 text-danger font-weight-bold" style={{cursor: 'pointer'}} onClick={() => setImg(img-1 < 1 ? 1 : img-1)}>{'<'}</h4>
+              <h6 style={{height: 'inherit', alignItems: 'center'}} className="d-flex m-0 p-2 text-warning">{img+'/'+imgs.length}</h6>
+              <h4 className="m-0 p-2 text-danger font-weight-bold" style={{cursor: 'pointer'}} onClick={() => setImg(img+1 > imgs.length ? 1 : img+1)}>{'>'}</h4>
             </div>
           </div>
         </div>
