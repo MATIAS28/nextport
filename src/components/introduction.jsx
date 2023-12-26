79bfb65b2b98714c14a292d3442f96d2e5f71563
MATIAS28 <matiasmuoz22@yahoo.com>
2023-12-26 12:04:59 -0300

    responsive update

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -12,7 +12,7 @@ export const IntroductionComponent = () => {
             <div className="md:p-1 w-full">
                 <div className="">
                     <h1 className="text-white text-2xl md:text-7xl font-semibold">Hola, soy</h1>
-                    <h2 className="titleName secondary text-2xl md:text-7xl font-semibold my-0">Matias Muñoz</h2>
+                    <h2 className="titleName secondary text-xl md:text-7xl font-semibold my-0">Matias Muñoz</h2>
                     <h3 className="text-white text-lg md:text-5xl font-semibold">Desarrollador Web</h3>
                 </div>
 
