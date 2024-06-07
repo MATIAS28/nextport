d58700010a9d8cee52ed9a50e3c4846953695be1
MATIAS28 <matiasmuoz22@yahoo.com>
2024-06-07 17:38:51 -0300

    skills-update

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -12,14 +12,14 @@ export const SkillsComponent = () => {
     return(
         <div id='skills' className="w-full h-full md:mb-12">
 
-                <div className="flex justify-center items-center mb-4">
+                <div className="flex md:justify-center items-center mb-4">
                     <CodeBracketIcon className="w-7 h-7 fill-yellow-300 mr-2"/>
                     <h2 className="text-2xl md:text-3xl text-white font-semibold">Habilidades</h2>
                 </div>
 
                 <div>
                 <div className="my-7">
-                    <p className="text-center text-gray-200 font-light text-sm md:text-lg mt-3 w-full">
+                    <p className="md:text-center text-gray-200 font-light text-sm md:text-lg mt-3 w-full">
                     Como desarrollador web fullstack especializado en el stack MERN, me he enfocado 
                     en poner a prueba mis habilidades en cada proyecto para fortalecerlas y mejorarlas 🚀. 
                     Actualmente, estoy explorando TypeScript y React Native 📚 para agregar nuevas habilidades 
